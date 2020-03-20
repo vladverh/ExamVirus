d6f184a57b7fdeea1cb801bb54919e12193d7909
Vladimir Verhooste <verhooste.vladimir@gmail.com>
2020-03-20 09:59:38 +0100

    first migration

---
+++ src/Migrations/Version20200320085858.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200320085858 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE country (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE stat (id INT AUTO_INCREMENT NOT NULL, country_id INT NOT NULL, contaminated INT DEFAULT NULL, healed INT DEFAULT NULL, zombified INT DEFAULT NULL, stat_date DATE NOT NULL, INDEX IDX_20B8FF21F92F3E70 (country_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE stat ADD CONSTRAINT FK_20B8FF21F92F3E70 FOREIGN KEY (country_id) REFERENCES country (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE stat DROP FOREIGN KEY FK_20B8FF21F92F3E70');
+        $this->addSql('DROP TABLE country');
+        $this->addSql('DROP TABLE stat');
+    }
+}
